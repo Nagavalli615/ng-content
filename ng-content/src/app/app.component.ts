b0899a5847a4776f695787d7162ae9191776323d
nagavallli <vallikatta226@gmail.com>
2025-01-23 14:56:43 +0530

    ngComponentoutlet

--- ng-content/src/app/app.component.ts
+++ ng-content/src/app/app.component.ts
@@ -1,14 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, ComponentRef, ViewContainerRef } from '@angular/core';
 //import { RouterOutlet } from '@angular/router';
 import { CardComponent } from './card/card.component';
 import { PosListComponent } from './pos-list/pos-list.component';
-
+import { NgComponentOutlet } from '@angular/common'
 @Component({
   selector: 'app-root',
-  imports: [CardComponent,PosListComponent],
+  imports: [CardComponent,PosListComponent,NgComponentOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'ng-content';
+  constructor(private viewContainerRef : ViewContainerRef){}
+
+  loadCompoent(){
+    return PosListComponent
+  }
+
+  loadcompoentbutton(){
+    this.viewContainerRef.createComponent(PosListComponent)
+  }
+  Destroycompoentbutton(){
+    this.viewContainerRef.remove();
+  }
 }
